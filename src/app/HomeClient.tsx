8e30ee30f45b41916ab06162d940701943eceaab
Z User <z@container>
2026-04-29 11:30:58 +0000

    6493ef8e-8440-49af-8377-b40cf4f1635c

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -8,12 +8,5 @@ export default function HomeClient() {
     window.location.replace('/home.html')
   }, [])
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="text-center">
-        <div className="w-16 h-16 border-4 border-gray-600 border-t-white rounded-full animate-spin mx-auto mb-4"></div>
-        <p className="text-white text-lg">Загрузка Century Intelligence...</p>
-      </div>
-    </div>
-  )
+  return null
 }
